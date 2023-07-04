8ca3d396ad063581f96ab15b89bdf33579934727
yasithwickramaarachchi <138301307+yasithwickramaarachchi@users.noreply.github.com>
2023-07-04 11:14:04 +0530

    Add files via upload

---
+++ 2023.07.04/Lab 6 - Q 6/Lab 6 - Q 6/ArrayOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6___Q_6
+{
+    internal class ArrayOperations
+    {
+        public void FillArray(int size)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("Enter value for index" + i + ": ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+                array[++i] = 0;
+            }
+            Console.WriteLine("Values in the array: ");
+            foreach (int num in array)
+            {
+                Console.WriteLine(num + " ");
+            }
+        }
+    }
+}
---
+++ 2023.07.04/Lab 6 - Q 6/Lab 6 - Q 6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6___Q_6
+{
+    internal class program
+    {
+        static void Main(string[] arrgs)
+        {
+            Console.Write("enter the size of the array: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            ArrayOperations arrayOps = new ArrayOperations();
+            arrayOps.FillArray(size);
+
+            Console.ReadLine();
+        }
+    }
+}
